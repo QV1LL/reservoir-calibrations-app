981a8abeeffe93fdf033857036e510966147e0bd
QV1LL <ghost228678@gmail.com>
2024-08-14 23:37:56 +0300

    Have tried to inflate some layout, begun work

--- app/src/main/java/com/example/reservoircalibrations/MainActivity.java
+++ app/src/main/java/com/example/reservoircalibrations/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.reservoircalibrations;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +15,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    LayoutInflater inflater;
+    Button inflateButton;
+
+    LinearLayout linearLayout;
+
+    List<View> inflatedItems = new ArrayList<View>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,28 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        linearLayout = (LinearLayout) findViewById(R.id.items_layout);
+
+        inflater = getLayoutInflater();
+        inflateButton = (Button) findViewById(R.id.inflate_button);
+        inflateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final View item = inflater.inflate(R.layout.item, null, false);
+                linearLayout.addView(item);
+
+                item.setClickable(true);
+                item.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        linearLayout.removeView(item);
+                    }
+                });
+
+                inflatedItems.add(item);
+            }
         });
     }
 }
